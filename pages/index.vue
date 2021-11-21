7a1900c8e13cda90c25a78df06bc6488f6850eaf
USER <EMAIL>
2021-11-21 19:26:08 +0700

    edit sql

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
       </v-col>
     </v-row>
     <h1 style="margin-top:20px;">
-      KNN BANK
+      TRS BANK
     </h1>
     <h3>MOBILE APP</h3>
     <v-row>
@@ -23,20 +23,128 @@
             sm="6"
             md="4"
           >
-            <v-text-field
-              label="PLEASE ENTER YOUR PASSWORD"
-              placeholder="PASSWORD"
-              filled
-              rounded
-              dense
-            />
+            <v-menu transition="scroll-y-transition">
+              <template #activator="{ on, attrs }">
+                <v-text-field
+                  label="PLEASE ENTER YOUR PASSWORD"
+                  placeholder="PASSWORD"
+                  filled
+                  rounded
+                  dense
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  />
+                </v-text-field>
+              </template>
+              <div class="box">
+                <v-row>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      1
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      2
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      3
+                    </v-btn>
+                  </v-col>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      4
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      5
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      6
+                    </v-btn>
+                  </v-col>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      7
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      8
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      9
+                    </v-btn>
+                  </v-col>
+                  <v-col class="d-flex justify-center ">
+                    <v-btn
+                      class="mx-3"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      0
+                    </v-btn>
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="yellow"
+                    >
+                      x
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-menu>
             <v-btn
               class="btn1"
             >
-              <span class="txt1">LOGIN</span>
+              <span class="txtt">LOGIN</span>
             </v-btn>
-            <p>Forgot password?</p>
-            <p>New to Bank Apps? sign Up</p>
+            <p>forgot password?</p>
+            <p>new to bank app? sigh up</p>
           </v-col>
         </v-form>
       </v-container>
@@ -44,19 +152,20 @@
   </div>
 </template>
 <style>
-.bg{
-  background: linear-gradient(to top right,#5842ff,#0989ff,#038fff);
+.pa-7{
   margin-top: 100px;
 }
-.h3{
+h3{
   margin-bottom: 50px;
-}
-.txt1{
-  color: brown;
 }
 .btn1{
   background: linear-gradient(to bottom,#ffeb3b,#ffc107);
   margin-bottom: 60px;
 }
-
+.box{
+ background-color: white;
+ padding: 5px;
+ border-radius: 25px;
+ font-size: 18px;
+}
 </style>
---
+++ pages/signup.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-parallax
+    dark
+    src="https://p0.pikist.com/photos/596/263/background-abstract-abstract-background-colorful-abstract-background-digital-style-light.jpg"
+  >
+    <h2> Sing up your bank account</h2>
+    <div>
+      <v-text-field
+        label="Your name"
+        hide-details="auto"
+      />
+      <v-text-field
+        label="Bank Account"
+        hide-details="auto"
+      />
+      <v-text-field
+        label="Mobile phone number"
+        hide-details="auto"
+      />
+      <v-text-field
+        label="Email"
+        hide-details="auto"
+      />
+      <v-text-field
+        label="Password"
+        hide-details="auto"
+      />
+    </div><br>
+    <v-checkbox
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="By signing up, you agree to Bank's Term of Use &amp; Privacy Policy."
+      required
+    />
+    <v-row>
+      <v-col class="d-flex justify-center ">
+        <v-btn class="btn1" color="yellow lighten-4">
+          <span style="color: brown">SIGN UP</span>
+        </v-btn> &nbsp; &nbsp; &nbsp;
+        <h3>or</h3> &nbsp; &nbsp; &nbsp;
+        <v-btn class="btn2" color="transparent">
+          <span>CANCEL</span>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <p>Already Signed up? Login</p>
+    </v-row>
+  </v-parallax>
+</template>
+<style>
+
+</style>
